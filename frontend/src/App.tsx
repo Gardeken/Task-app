29fb6f6e04e94d92e26f9b4024beb6db6681ea50
Gardeken <gardeken931@gmail.com>
2026-06-22 02:38:20 -0400

    git commit -m "feat: setup frontend with React, TypeScript, and Tailwind CSS"

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Trash2 } from "lucide-react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Trash2 /> Borra mundo{" "}
+    </div>
+  );
+}
+
+export default App;
